a52dc98ee7b93b18cde3e36a6363e3c4220d6a56
simonwisseler <simonwisseler@gmail.com>
2019-03-09 01:17:12 +0100

    Add UKF Constructor and UKF::ProcessMeasurement in ukf.cpp

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -4,65 +4,115 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
+using namespace std;
+
+
 /**
  * Initializes Unscented Kalman filter
  */
 UKF::UKF() {
-  // if this is false, laser measurements will be ignored (except during init)
-  use_laser_ = true;
+    // if this is false, laser measurements will be ignored (except during init)
+    use_laser_ = true;
 
-  // if this is false, radar measurements will be ignored (except during init)
-  use_radar_ = true;
+    // if this is false, radar measurements will be ignored (except during init)
+    use_radar_ = true;
 
-  // initial state vector
-  x_ = VectorXd(5);
+    // state dimension
+    n_x_ = 5;
 
-  // initial covariance matrix
-  P_ = MatrixXd(5, 5);
+    // initial state vector
+    x_ = VectorXd(n_x_);
 
-  // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+    // initial covariance matrix
+    P_ = MatrixXd(n_x_, n_x_);
+    P_ <<   0.1, 0, 0, 0, 0,
+            0, 0.1, 0, 0, 0,
+            0, 0, 0.2, 0, 0,
+            0, 0, 0, 1.0, 0,
+            0, 0, 0, 0, 1.0;
 
-  // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
-  
-  /**
-   * DO NOT MODIFY measurement noise values below.
-   * These are provided by the sensor manufacturer.
-   */
+    // process noise standard deviation longitudinal acceleration in m/s^2
+    std_a_ = 1.5;
 
-  // Laser measurement noise standard deviation position1 in m
-  std_laspx_ = 0.15;
+    // process noise standard deviation yaw acceleration in rad/s^2
+    std_yawdd_ = 0.3;
 
-  // Laser measurement noise standard deviation position2 in m
-  std_laspy_ = 0.15;
+    // augmented state dimension
+    n_aug_ = n_x_ + 2;
+    
+    // sigma point spreading parameter --> heuristic from Julier, S., and J. Uhlmann. 1997. A new extension of the Kalman filter to nonlinear systems
+    lambda_ = 3 - n_x_;
+    
+    // weights of sigma points
+    weights_ = VectorXd(2*n_aug_ + 1);
+    weights_.fill(0.5/(n_aug_ + lambda_));
+    weights_(0) = lambda_/(lambda_ + n_aug_);
 
-  // Radar measurement noise standard deviation radius in m
-  std_radr_ = 0.3;
+    // predicted sigma points matrix
+    Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);
+    
+    // laser measurement noise standard deviation position1 in m
+    std_laspx_ = 0.15;
 
-  // Radar measurement noise standard deviation angle in rad
-  std_radphi_ = 0.03;
+    // laser measurement noise standard deviation position2 in m
+    std_laspy_ = 0.15;
 
-  // Radar measurement noise standard deviation radius change in m/s
-  std_radrd_ = 0.3;
-  
-  /**
-   * End DO NOT MODIFY section for measurement noise values 
-   */
-  
-  /**
-   * TODO: Complete the initialization. See ukf.h for other member properties.
-   * Hint: one or more values initialized above might be wildly off...
-   */
+    // radar measurement noise standard deviation radius in m
+    std_radr_ = 0.3;
+
+    // radar measurement noise standard deviation angle in rad
+    std_radphi_ = 0.03;
+
+    // radar measurement noise standard deviation radius change in m/s
+    std_radrd_ = 0.3;
+
+    // laser measurement noise covariance matrix
+    R_laser_ = MatrixXd(2, 2);
+    R_laser_ << std_laspx_*std_laspx_,0,
+                0,std_laspy_*std_laspy_;
+    
+    // radar measurement noise covariance matrix
+    R_radar_ = MatrixXd(3, 3);
+    R_radar_ << std_radr_*std_radr_, 0, 0,
+                0, std_radphi_*std_radphi_, 0,
+                0, 0,std_radrd_*std_radrd_;
+    
 }
 
 UKF::~UKF() {}
 
 void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
-  /**
-   * TODO: Complete this function! Make sure you switch between lidar and radar
-   * measurements.
-   */
+    
+    if (!is_initialized_) {
+        
+        if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
+            x_ << meas_package.raw_measurements_[0], meas_package.raw_measurements_[1], 0, 0, 0;
+        } else {
+            double rho = meas_package.raw_measurements_[0]; // range
+            double phi = meas_package.raw_measurements_[1]; // bearing
+            double rho_dot = meas_package.raw_measurements_[2]; // range rate (https://en.wikipedia.org/wiki/Range_rate)
+            
+            double x = rho * cos(phi);
+            double y = rho * sin(phi);
+            // best estimate for v as component of velocity normal to rho not captured by radar measurement
+            double v = rho_dot
+            x_ << x, y, v, 0, 0;
+        }
+        previous_timestamp_ = meas_package.timestamp_ ;
+        is_initialized_ = true;
+        return;
+    }
+    
+    float dt = (meas_package.timestamp_ - previous_timestamp_) / 1000000.0; // time (in sec) elapsed between current and previous measurement
+    previous_timestamp_ = meas_package.timestamp_;
+    
+    Prediction(dt);
+    
+    if (meas_package.sensor_type_ == MeasurementPackage::LASER && use_laser_) {
+        UpdateLidar(meas_package);
+    }
+    else if (meas_package.sensor_type_ == MeasurementPackage::RADAR && user_radar_) {
+        UpdateRadar(meas_package);
 }
 
 void UKF::Prediction(double delta_t) {
